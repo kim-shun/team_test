c34cea6b8cb161b4ef1380bfec0bb811e403acee
kim-shun <kimura1417080@gmail.com>
2021-06-22 17:13:16 +0900

    クラスの継承

--- fruit.rb
+++ fruit.rb
@@ -23,29 +23,70 @@
 #  orange.introduce
 #  strawbery.introduce
 
+# class Car
+
+#   def self.sell
+#     puts "こちらの車を販売しています"
+#   end
+
+#   def initialize(name, price)
+#     @car_name = name
+#     @car_price = price
+#   end
+ 
+#   def introduce
+#     puts "#{@car_name}は#{@car_price}円です"
+#   end
+# end
+
+# Car.sell
+
+# tanto = Car.new("タント", 5000000)
+# n_box = Car.new("N-BOX", 6000000)
+# hustler = Car.new("ハスラー", 7000000)
+
+# tanto.introduce
+# n_box.introduce
+# hustler.introduce
+
+# class Car
+#   def initialize
+#     puts "こちらの車を販売しています"
+#   end
+# end
+
+# Car.new 
+
+
+#クラスの継承
+
 class Car
-
-  def self.sell
-    puts "こちらの車を販売しています"
-  end
-
-  def initialize(name, price)
-    @car_name = name
-    @car_price = price
+  def initialize(car_type, capacity)
+    @name = car_type
+    @capacity = capacity
   end
  
-  def introduce
-    puts "#{@car_name}は#{@car_price}円です"
+  def info
+    puts "車種：#{@name}　乗車定員：#{@capacity}人"
   end
-end
-
-Car.sell
-
-tanto = Car.new("タント", 5000000)
-n_box = Car.new("N-BOX", 6000000)
-hustler = Car.new("ハスラー", 7000000)
-
-tanto.introduce
-n_box.introduce
-hustler.introduce
-
+ 
+ end
+ 
+ class PoliceCar < Car
+ 
+  def info
+    puts "車種：#{@name}　乗車定員：#{@capacity}人　パトロール時間：24時間"
+  end
+ 
+  def siren
+    puts "ウゥ〜ウゥ〜"
+  end
+ 
+ end
+ 
+ police_car = PoliceCar.new("パトカー", 5)
+ 
+ police_car.info
+ 
+ # ターミナル出力結果
+ # 車種：パトカー　乗車定員：5人　パトロール時間：24時間
