5bb7c538b765a4efd582045c481451d8a5151cdb
kim-shun <kimura1417080@gmail.com>
2021-06-24 15:32:25 +0900

    123が含まれているかどうか

---
+++ array123.rb
@@ -0,0 +1,13 @@
+def array123(nums)
+  if nums.include?(1) && nums.include?(2) && nums.include?(3)
+    puts "True"
+  else
+    puts "False"
+  end
+end
+
+# 呼び出し例
+array123([1, 1, 2, 3, 1])
+array123([1, 1, 2, 1, 7])
+array123([1, 1, 8, 3, 1])
+array123([6, 4, 2, 3, 1])
