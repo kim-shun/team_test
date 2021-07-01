edaf5eb41fcd6ad92a61e9bf81b547e5a338bdad
kim-shun <kimura1417080@gmail.com>
2021-07-02 08:51:49 +0900

    閏年問題

---
+++ get_days.rb
@@ -0,0 +1,39 @@
+def get_days(year, month)
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if month == 2
+    if year % 4 == 0
+      if year % 100 == 0 && year % 400 == 0
+        days = 29
+      else
+        days = month_days[1]
+      end
+    else
+      days = month_days[1]
+    end
+  else
+    days = month_days[month - 1]
+  end
+  puts "#{year}年#{month}月は#{days}日間あります"
+end
+
+days = get_days(1990,2)
+days = get_days(2000,2)
+days = get_days(2100,2)
+days = get_days(2000,3)
+days = get_days(2021,7)
+days = get_days(2021,8)
+days = get_days(2021,6)
+
+
+# puts "年を入力してください："
+# year = gets.to_i
+# puts "月を入力してください："
+# month = gets.to_i
+
+# days = get_days(year, month)
+# puts "#{year}年#{month}月は#{days}日間あります"
+
+# 1990年2月 =>"1990年2月は28日間あります"
+# 2000年2月 =>"2000年2月は29日間あります"
+# 2100年2月 =>"2100年2月は28日間あります"
+# 2000年3月=>"2000年3月は31日間あります"
