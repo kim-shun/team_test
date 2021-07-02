e9cff7deea6923ead849479f7ec80a049a63d7b2
kim-shun <kimura1417080@gmail.com>
2021-07-02 16:35:03 +0900

    成績の登録

--- get_days.rb
+++ get_days.rb
@@ -1,28 +1,28 @@
-def get_days(year, month)
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  if month == 2
-    if year % 4 == 0
-      if year % 100 == 0 && year % 400 == 0
-        days = 29
-      else
-        days = month_days[1]
-      end
-    else
-      days = month_days[1]
-    end
-  else
-    days = month_days[month - 1]
-  end
-  puts "#{year}年#{month}月は#{days}日間あります"
-end
+# def get_days(year, month)
+#   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#   if month == 2
+#     if year % 4 == 0
+#       if year % 100 == 0 && year % 400 == 0
+#         days = 29
+#       else
+#         days = month_days[1]
+#       end
+#     else
+#       days = month_days[1]
+#     end
+#   else
+#     days = month_days[month - 1]
+#   end
+#   puts "#{year}年#{month}月は#{days}日間あります"
+# end
 
-days = get_days(1990,2)
-days = get_days(2000,2)
-days = get_days(2100,2)
-days = get_days(2000,3)
-days = get_days(2021,7)
-days = get_days(2021,8)
-days = get_days(2021,6)
+# days = get_days(1990,2)
+# days = get_days(2000,2)
+# days = get_days(2100,2)
+# days = get_days(2000,3)
+# days = get_days(2021,7)
+# days = get_days(2021,8)
+# days = get_days(2021,6)
 
 
 def get_days(year, month)
