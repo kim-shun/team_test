46e6daba4c64c455c8c51e0adf0aae3337edf3d9
kim-shun <kimura1417080@gmail.com>
2021-07-07 12:47:52 +0900

    カレンダーの作成の途中

---
+++ calender.rb
@@ -0,0 +1,35 @@
+require "date"
+
+puts "#{Date.today.strftime('%B')} #{Date.today.strftime('%Y')}"
+
+puts Date.today.strftime('%a')
+year = Date.today.year
+mon = Date.today.mon
+puts year
+puts mon
+days = ["Su","Mo","Tu","We","Th","Fr","Sa"]
+first = Date.new(year,mon,1).wday
+last = Date.new(year,mon,-1).day
+puts first
+puts last
+
+a = Date.new
+30.times do 
+  b = a.strftime('%e')
+  p b.ljust(7)
+  a += 1
+end
+
+s = "Ruby"
+ p s.center(10) 
+
+
+# 出力例
+#      April 2013
+# Su Mo Tu We Th Fr Sa
+#     1  2  3  4  5  6
+#  7  8  9 10 11 12 13
+# 14 15 16 17 18 19 20
+# 21 22 23 24 25 26 27
+# 28 29 30
+
