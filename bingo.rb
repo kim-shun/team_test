313af2d944d32887340888660164453e78935708
kim-shun <kimura1417080@gmail.com>
2021-07-09 17:21:45 +0900

    単純な理屈

--- bingo.rb
+++ bingo.rb
@@ -1,47 +1,55 @@
-puts "  B |  I |  N |  G |  O"
+# puts "  B |  I |  N |  G |  O"
 
-def rand_b
-  b = rand(1..15)
-  if b <= 9
-    print "  #{b} |"
-  else
-    print " #{b} |"
-  end
-end
+# def rand_b
+#   b = rand(1..15)
+#   if b <= 9
+#     print "  #{b} |"
+#   else
+#     print " #{b} |"
+#   end
+# end
 
-def rand_i
-  i = rand(16..30)
-  print " #{i} |"
-end
+# def rand_i
+#   i = rand(16..30)
+#   print " #{i} |"
+# end
 
-def rand_n
-  n = rand(31..45)
-  print " #{n} |"
-end
+# def rand_n
+#   n = rand(31..45)
+#   print " #{n} |"
+# end
 
-def rand_g
-  g= rand(46..60)
-  print " #{g} |"
-end
+# def rand_g
+#   g= rand(46..60)
+#   print " #{g} |"
+# end
 
-def rand_o
-  o = rand(61..75)
-  print " #{o} "
-end
+# def rand_o
+#   o = rand(61..75)
+#   print " #{o} "
+# end
 
-5.times do |count|
-  rand_b
-  rand_i
-  if count == 2
-    print "    |"
-  else
-    rand_n
-  end
-  rand_g
-  rand_o
+# 5.times do |count|
+#   rand_b
+#   rand_i
+#   if count == 2
+#     print "    |"
+#   else
+#     rand_n
+#   end
+#   rand_g
+#   rand_o
+#   puts "\n"
+# end
+
+5.times do
+  print rand(1..15)
+  print rand(16..30)
+  print rand(31..45)
+  print rand(46..60)
+  print rand(1..75)
   puts "\n"
 end
-
 
 #回答例
 # class Bingo
