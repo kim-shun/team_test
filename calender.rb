4fe268730fafd419164af413bee804e497eb4b10
kim-shun <kimura1417080@gmail.com>
2021-07-09 15:13:57 +0900

    カレンダー作成問題

--- calender.rb
+++ calender.rb
@@ -1,23 +1,49 @@
 require "date"
 
-puts "#{Date.today.strftime('%B')} #{Date.today.strftime('%Y')}"
+year_month =  "#{Date.today.strftime('%B')} #{Date.today.strftime('%Y')}"
+puts year_month.center(20)
+this_year = Date.today.year
+this_month = Date.today.month
+first_day = Date.new(this_year, this_month,1)
+last_day = Date.new(this_year, this_month,-1)
+wday_first = first_day.wday
+blank = "   " * wday_first
 
-week_days = ["Su","Mo","Tu","We","Th","Fr","Sa"]
+puts "Su Mo Tu We Th Fr Sa"
+print blank
+(first_day.day..last_day.day).each{ |day|
+  if day <= 9
+    print " #{day} "
+  else
+    print "#{day} "
+  end
 
-puts week_days.join(" ")
-
-year = Date.today.year
-month = Date.today.month
-first_day = Date.new(year, month, 1)
-last_day = Date.new(year, month, -1)
-
-
-(first_day.day..last_day.day).each{|day|
-  print "#{day}  "
-  if day % 7 == 0
-    print "\n"
+  if (wday_first + day) % 7 == 0
+    puts "\n"
   end
 }
+
+
+# week_days = ["Su","Mo","Tu","We","Th","Fr","Sa"]
+
+# puts week_days.join(" ")
+
+# year = Date.today.year
+# month = Date.today.month
+# first_day = Date.new(year, month, 1)
+# last_day = Date.new(year, month, -1)
+
+
+# (first_day.day..last_day.day).each{|day|
+#   if day <= 9
+#     print " #{day} "
+#   else
+#     print "#{day} "
+#   end
+#   if day % 7 == 0
+#     print "\n"
+#   end
+# }
 
 
 
