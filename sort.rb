f9a11b9fd1490c88269977c389da63ed35e9b8af
kim-shun <kimura1417080@gmail.com>
2021-07-12 08:53:30 +0900

    カタカナの名前もいける

--- sort.rb
+++ sort.rb
@@ -1,7 +1,11 @@
 ary = [4, 2, 5, 1, 3]
 newary = ary.sort
 p newary
+#=> [1, 2, 3, 4, 5]
 
 array = ["か", "そ", "わ", "に", "あ"]
-kanarray = array.sort
-p kanarray
+p array.sort
+#=> ["あ", "か", "そ", "に", "わ"]
+
+names = ['キシモト', 'イトウ', 'ババ', 'カネダ', 'ワダ', 'ハマダ']
+p names.sort
