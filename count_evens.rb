da7528866ae44c25fa8f28b240eeb4faf5301748
kim-shun <kimura1417080@gmail.com>
2021-08-07 09:33:40 +0900

    コメントアウトを変える

--- count_evens.rb
+++ count_evens.rb
@@ -1,22 +1,22 @@
-def count_evens(nums)
-  even = []
-  nums.each do |num|
-    if num.even?
-      even << num
-    end
-  end
-  puts even.length
-end
-
 # def count_evens(nums)
-#   count = 0
+#   even = []
 #   nums.each do |num|
 #     if num.even?
-#       count += 1
-#     end     
+#       even << num
+#     end
 #   end
-#   puts count
+#   puts even.length
 # end
+
+def count_evens(nums)
+  count = 0
+  nums.each do |num|
+    if num.even?
+      count += 1
+    end     
+  end
+  puts count
+end
 
 count_evens([2, 1, 7, 8, 2, 3, 4])
 count_evens([2, 2, 6, 5, 0]) 
