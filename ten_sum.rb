d6d2a7f5a77feb99650f64c2ff52a1abe29859f3
kim-shun <kimura1417080@gmail.com>
2021-08-12 09:07:10 +0900

    10の倍数に近い数字

--- ten_sum.rb
+++ ten_sum.rb
@@ -16,23 +16,23 @@
 
 
 
-# def near_ten(num)
-#   total = (num/100) + (num/10 % 10) + (num % 10)
-#   remainder = total % 10
-#   if remainder <= 2 || remainder >= 8
-#     puts "True"
-#   elsif remainder <= 5
-#     puts "10の倍数との差は#{remainder}です"
-#   else 
-#     puts "10の倍数との差は#{10 - remainder}です"
-#   end
-# end
+def near_ten(num)
+  total = (num/100) + (num/10 % 10) + (num % 10)
+  remainder = total % 10
+  if remainder <= 2 || remainder >= 8
+    puts "True"
+  elsif remainder <= 5
+    puts "10の倍数との差は#{remainder}です"
+  else 
+    puts "10の倍数との差は#{10 - remainder}です"
+  end
+end
 
-# near_ten(117) # True
-# near_ten(123) # 10の倍数との差は4です
-# near_ten(111) # 10の倍数との差は3です
-# near_ten(98) # 10の倍数との差は3です
-# near_ten(124) # 10の倍数との差は3です
-# near_ten(400) # 10の倍数との差は4です
-# near_ten(45) # True
+near_ten(117) # True
+near_ten(123) # 10の倍数との差は4です
+near_ten(111) # 10の倍数との差は3です
+near_ten(98) # 10の倍数との差は3です
+near_ten(124) # 10の倍数との差は3です
+near_ten(400) # 10の倍数との差は4です
+near_ten(45) # True
 
